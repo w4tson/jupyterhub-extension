cb8ca662ff0ed79a103d455f3973590c6b48c0ea
watson <paul@hashbang.digital>
2024-02-11 11:11:43 +0000

    init

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { ISettingRegistry } from '@jupyterlab/settingregistry';
+
+/**
+ * Initialization data for the myextension extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'myextension:plugin',
+  description: 'A JupyterLab extension.',
+  autoStart: true,
+  optional: [ISettingRegistry],
+  activate: (app: JupyterFrontEnd, settingRegistry: ISettingRegistry | null) => {
+    console.log('JupyterLab extension myextension is activated!');
+
+    if (settingRegistry) {
+      settingRegistry
+        .load(plugin.id)
+        .then(settings => {
+          console.log('myextension settings loaded:', settings.composite);
+        })
+        .catch(reason => {
+          console.error('Failed to load settings for myextension.', reason);
+        });
+    }
+  }
+};
+
+export default plugin;
